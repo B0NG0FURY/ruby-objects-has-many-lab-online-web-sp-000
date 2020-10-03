a235d8935f4e9b026d0b7e1a71c60c8baf2e9735
John Troutman <jtroutmanman@gmail.com>
2020-10-03 16:56:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,6 +9,11 @@ class Author
     post.author = self
   end
   
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+  
   def posts
     Post.all.select {|post| post.author == self}
   end
