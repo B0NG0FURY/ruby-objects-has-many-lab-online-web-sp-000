0a404479e5eefe2577afc815502f6345fcccf3d6
John Troutman <jtroutmanman@gmail.com>
2020-10-03 16:36:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
-  attr_accessor :title, :genre, :artist
+  attr_accessor :name, :genre, :artist
   
   @@all = []
   
-  def initialize(title, genre)
-    @title = title
+  def initialize(name, genre)
+    @name = name
     @genre = genre
     save
   end
